0da42b70d84e7b7fe7bfbba7ef7d38cced3c1e04
Faith yator <you@example.com>
2023-07-03 10:43:36 +0300

    change

--- src/pages/Contactform.jsx
+++ src/pages/Contactform.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import './ContactForm.css';
+import './Contactform.css';
 import Axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
